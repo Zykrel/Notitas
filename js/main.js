8be42989273e90175f2b4cc9ca673ef858b0f633
Zykrel <kamaelnahuel@gmail.com>
2022-02-25 00:28:02 -0300

    imagenes

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const imagenes = [
     'multimedia/pp (1).png', 
     'multimedia/0228e999-4394-43f0-a762-823ff939cef6.jpg',
     'multimedia/f90b0a25f35a0357b28c25c5e8a9364f.jpg',
-    'multimedia/da.jpg'
+    'multimedia/da.jpg',
+    'multimedia/IMG_20211105_213033478.jpg',
+    'multimedia/IMG_20211105_213040897.jpg',
+    'multimedia/IMG_20211231_215300868_HDR.jpg',
+    'multimedia/IMG_20220108_202931106.jpg',
+    'multimedia/IMG_20220108_202954228.jpg',
+    'multimedia/IMG_20220206_191302109_HDR.jpg',
+    'multimedia/IMG_20220206_191319765_HDR.jpg',
+    'multimedia/IMG_20220207_112652265.jpg',
+    'multimedia/IMG_20220207_112653725.jpg',
+    'multimedia/IMG_20220207_112701323_HDR.jpg',
 ];
 
 const links = [
